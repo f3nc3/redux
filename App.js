b7aec7b7a11c0ddc6fee31db3b1b74915cb022fd
f3nc3 <168217860+f3nc3@users.noreply.github.com>
2024-10-20 21:45:23 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import Counter from './Counter';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Redux Counter App</h1>
+      <Counter />
+    </div>
+  );
+};
+
+export default App;
---
+++ r-action.js
@@ -0,0 +1,16 @@
+// src/redux/actions.js
+export const INCREMENT = 'INCREMENT';
+export const DECREMENT = 'DECREMENT';
+export const RESET = 'RESET';
+
+export const increment = () => ({
+  type: INCREMENT,
+});
+
+export const decrement = () => ({
+  type: DECREMENT,
+});
+
+export const reset = () => ({
+  type: RESET,
+});
